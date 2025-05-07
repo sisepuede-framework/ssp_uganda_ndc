0fdc66fadec40baa26d8b156d691e2921f635274
fabian_fuentes <cffuentes@tec.mx>
2025-05-07 11:41:57 -0600

    post processing bau energy

---
+++ ssp_modeling/output_postprocessing/postprocessing_process_bau_energy.r
@@ -0,0 +1,16 @@
+#################################################
+# Post processing process
+#################################################
+
+# load packages
+library(data.table)
+
+
+rm(list=ls())
+
+source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new_bau.r')
+
+source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping_bau_only.r')
+
+
+
